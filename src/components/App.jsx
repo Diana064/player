48f7cad33016436bd2fe7bf59f5fc49b8d66dc1c
Diana064 <dipustolyakova02@gmail.com>
2023-01-12 21:29:19 +0200

    add components

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Player</h1>
+      </div>
+    );
+  }
+}
+export default App;
